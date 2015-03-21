0d12e914161b1530703173337b7d6f66f4c79822
Steven Dwy <yoplitein+git@gmail.com>
2015-03-21 15:14:33 -0700

    Fix errors in init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 sdlbase="https://hg.libsdl.org/"
-ghbase="ssh://git@github.com/SDL-mirror/"
+gitbase="ssh://git@github.com/SDL-mirror/"
 
 for repo in `cat $(dirname $0)/repos.txt`; do
-    hg clone $base$repo
+    hg clone $sdlbase$repo
     mkdir $repo-git
     cd $repo-git
     git init --bare
-    git remote add origin $ghbase$repo
+    git remote add origin $gitbase$repo
     cd ..
 done
