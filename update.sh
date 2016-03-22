69880d13c32c763c6dd6599ea00d97d774b9375e
Steven Dwy <yoplitein+git@gmail.com>
2016-03-22 12:02:17 -0700

    Add verbose mode to update script

--- update.sh
+++ update.sh
@@ -3,17 +3,32 @@
 tools=$(dirname $0)
 log=`mktemp`
 
-function check()
-{
-    $@ >> $log 2>&1
-    
-    if [ $? != 0 ]; then
-        echo "Update failed:"
-        cat $log
-        rm -f $log
-        exit 1
-    fi
-}
+if [ "$1" == "-v" ]; then
+    function check()
+    {
+        echo $@
+        $@
+        
+        ret=$?
+        
+        if [ $ret != 0 ]; then
+            echo "Exited with code $ret"
+            exit 1
+        fi
+    }
+else
+    function check()
+    {
+        $@ >> $log 2>&1
+        
+        if [ $? != 0 ]; then
+            echo "Update failed:"
+            cat $log
+            rm -f $log
+            exit 1
+        fi
+    }
+fi
 
 check $tools/pull.sh
 check $tools/sync-bookmarks.sh
